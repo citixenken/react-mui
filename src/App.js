b617d9dfe0a45b7989afaec7d66d8918df6a3f29
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-01 22:47:35 +0300

    React + MUI project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Sidebar } from "./components/Sidebar";
+import { Feed } from "./components/Feed";
+import { Rightbar } from "./components/Rightbar";
+
+function App() {
+  return (
+    <div>
+      <Sidebar />
+      <Feed />
+      <Rightbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: { main: "#1760a5", light: "skyblue" },
+  secondary: { main: "#15c630" },
+  otherColor: { main: "#999" },
+});
